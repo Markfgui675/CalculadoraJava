3bccfe7782b6639cbef5e08b6549330e694a129a
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-10-01 20:51:57 -0300

    Crição de método para a troca de tela (Splash - Main).

--- app/src/main/java/com/example/calculadora1/SplashActivity.java
+++ app/src/main/java/com/example/calculadora1/SplashActivity.java
@@ -2,13 +2,35 @@ package com.example.calculadora1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    int tempoEspera = 1000 * 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        trocarTela();
+
+    }
+
+    private void trocarTela() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent trocarTela = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(trocarTela);
+                finish();
+
+            }
+        }, tempoEspera);
+
     }
 }
