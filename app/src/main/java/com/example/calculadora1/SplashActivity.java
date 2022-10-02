771ca5933026407b4ab941a13951bbf0c57e5875
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-10-01 21:00:09 -0300

    Some errors

--- app/src/main/java/com/example/calculadora1/SplashActivity.java
+++ app/src/main/java/com/example/calculadora1/SplashActivity.java
@@ -2,10 +2,12 @@ package com.example.calculadora1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     int tempoEspera = 1000 * 5;
